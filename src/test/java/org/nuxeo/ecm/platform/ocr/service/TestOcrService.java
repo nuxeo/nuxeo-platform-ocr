6bec4779a6837ea7fc98fdc316ab421509ab1ff3
TAN Sun Seng David <sunix@sunix.org>
2013-07-12 15:22:33 +0200

    fixing test compilation issues

--- src/test/java/org/nuxeo/ecm/platform/ocr/service/TestOcrService.java
+++ src/test/java/org/nuxeo/ecm/platform/ocr/service/TestOcrService.java
@@ -16,6 +16,11 @@
  */
 package org.nuxeo.ecm.platform.ocr.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.InputStream;
 import java.util.List;
 
